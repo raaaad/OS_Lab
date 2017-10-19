fc1da1dc0ff9d61979e08e5604509c37d0fb56c6
yyr <scsse_yyr2015@126.com>
2017-10-19 21:28:06 +0800

    finish prompt_v1

--- mysh.c
+++ mysh.c
@@ -1,8 +1,66 @@
 #include <stdio.h>
-#include <stdlib.h>
-int main()
-{
-		
-	return 0;
+#include <stdlib.h>//exit()
+#include <unistd.h>//fork();getpid();close();execvp()
+#include <string.h>
+#include <sys/types.h>//getuid()
+#include <pwd.h>//getpwuid();
+#define nameLen 50
+#define pathLen 1024
+#define MAX_CMD 512
+char prompt[MAX_CMD];
+//struct passwd *pwd;
+
+void type_prompt(char *prompt) {
+	struct passwd *pwd;
+	char userName[nameLen],hostName[nameLen],pathName[pathLen];
+	
+	pwd = getpwuid(getuid());
+	getcwd(pathName, pathLen);//get path
+
+	//print hostName and userName 
+    if(gethostname(hostName, nameLen) == 0)
+        sprintf(prompt, "mysh>%s@%s:",pwd->pw_name,hostName);
+    else
+        sprintf(prompt, "mysh>%s@unknown:",pwd->pw_name);
+    
+    //printf("pathname: %s,length:%d\npw_dir:%s,length:%d\n",pathName,(int)strlen(pathName),pwd->pw_dir,(int)strlen(pwd->pw_dir));
+    
+    int length = strlen(prompt);
+    
+    
+    if(strlen(pathName) < strlen(pwd->pw_dir) || strncmp(pathName, pwd->pw_dir, strlen(pwd->pw_dir)) != 0)
+        sprintf(prompt+length, "%s", pathName);
+    else
+        sprintf(prompt+length, "~%s", pathName+strlen(pwd->pw_dir));
+    
+    length = strlen(prompt);
+    if(geteuid() == 0)
+        sprintf(prompt+length,"# ");//root user
+    else
+        sprintf(prompt+length,"$ ");
+    puts(prompt);
+
 }
 
+int main(int argc, char *argv[]) {
+	
+	while(1) {
+		type_prompt(prompt);//print mysh>...
+		//read_command(commad,parameters);//get input
+
+		/*int rc = fork();
+	    if (rc < 0) {
+	    	//fork failed then exit
+			fprintf(stderr, "fork failed\n");
+	        exit(1);
+	    }
+		else if(rc != 0) {
+			//parent
+			waitpid(-1, &status, 0);//wait until child exit
+		}
+		else {
+			//child
+			execve(command, parameters, 0);//execute commad
+	*/}
+    return 0;
+}

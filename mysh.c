55d241e64ec39c7066481e69bc38f24190f4b03e
yyr <scsse_yyr2015@126.com>
2017-10-23 09:24:12 +0800

    complete the command reading

--- mysh.c
+++ mysh.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>//exit()
 #include <unistd.h>//fork();getpid();close();execvp()
-#include <string.h>
+#include <string.h>//strlen();
 #include <sys/types.h>//getuid()
 #include <pwd.h>//getpwuid();
 
@@ -11,6 +11,12 @@
 #define MAX_NAME 50
 #define MAX_PATH 1024
 #define MAX_CMD 512
+#define MAX_ARG 20
+
+#define DEBUG
+
+char *myshBuffer;
+
 
 void type_prompt(char* prompt) {
 	struct passwd *pwd;
@@ -34,39 +40,117 @@ void type_prompt(char* prompt) {
         length += sprintf(prompt+length, "~%s", pathName + strlen(pwd->pw_dir));
 
     length += sprintf(prompt+length, " mysh> ");
-    puts(prompt);
+    //puts(prompt);
     return;
 }
 
 //return value: number of parameters
 //0 represents only command without any parameters
-//-1 represents wrong input
-// int read_command(char** command, char** parameters, char* prompt)
-// {
-// 	#ifdef READLINE_ON
-//     	free(buffer);
-//     buffer = readline(prompt);
-//     if(feof(stdin)) {
-//         printf("\n");
-//         exit(0);
-//     }
+int read_command(char **command,char **parameters,char *prompt) {
+	printf("%s",prompt);
+    
+    char* cmd = fgets(myshBuffer,MAX_CMD,stdin);
+    
+    if (cmd == NULL) {
+        printf("\n");
+        exit(0);
+    }
 
-// 	#else
-// 	    printf("%s",prompt);
-// 	    char* Res_fgets = fgets(buffer,MAXLINE,stdin);
-// 	    if(Res_fgets == NULL)
-// 	    {
-// 	        printf("\n");
-// 	        exit(0);
-// 	    }		
-// 	#endif
-// }
+    if(myshBuffer[0] == '\0')
+        return -1;
 
-int main(int argc, char *argv[]) {
+    char *cmdHead,*cmdTail;
+    int count = 0;
+    int isFinished = 0;
+    
+    cmdHead = cmdTail = myshBuffer;//point to command's head
+    
+    while(isFinished == 0) {
+    	//deal with command's head
+        while((*cmdTail == ' ' && *cmdHead == ' ') || (*cmdTail == '\t' && *cmdHead == '\t')) {
+        	//leapfrog space and tab at command's head
+            cmdHead++;
+            cmdTail++;
+        }
+
+        if(*cmdTail == '\0' || *cmdTail == '\n') {
+            if(count == 0)//get a line break at command's head,exit
+                return -1;
+            break;
+        }
+
+        //move the cmdTail
+        while(*cmdTail != ' ' && *cmdTail != '\0' && *cmdTail != '\n')
+            cmdTail++;//pointers select a word without space
+
+        if(count == 0) { //to handle cd
+            char *p = cmdTail;
+            *command = cmdHead;
+            while(p != cmdHead && *p !='/')
+                p--;
+            if(*p == '/')
+                p++;//ignore '/''
+            parameters[0] = p;
+            count += 2;
+
+            #ifdef aaa
+            printf("\nHead:%s\nTail:%s\n",cmdHead,cmdTail);
+        	printf("\ncount==%d\ncommand:%s\n",count,*command);
+        	#endif
+        }
+
+        else if(count <= MAX_ARG) {
+            parameters[count-1] = cmdHead;
+            count++;
+        }
+        else
+        {
+            break;
+        }
+
+        //ready for next parameter
+        if(*cmdTail == '\0' || *cmdTail == '\n')
+        {
+            *cmdTail = '\0';
+            isFinished = 1;//to break
+        }
+        else {
+            *cmdTail = '\0';
+            cmdTail++;
+			cmdHead = cmdTail;//to continue
+        }
+    }
+
+    #ifdef DEBUG
+    printf("count:%d\n",count);
+    #endif
+    parameters[count-1] = NULL;
+
+    #ifdef DEBUG
+    printf("debug:\n");
+    printf("pathname:[%s]\ncommand:[%s]\nparameters:\n",*command,parameters[0]);
+    int i;
+    for(i = 0;i < count - 1; i++)
+        printf("[%s]\n", parameters[i]);
+    #endif
+
+    return count;
+}
+
+int main() {
 	char prompt[MAX_PROMPT];
-	//while(1) {
-		type_prompt(prompt);//print mysh>
-		//read_command(commad, parameters, prompt);//cmd input
+	char *command = NULL;
+	char **parameters;
+	int para_count = 0;
+
+	myshBuffer = malloc(sizeof(char)*MAX_CMD);
+	parameters = malloc(sizeof(char *)*MAX_ARG);
+	
+	while(1) {
+		type_prompt(prompt);//generate prompt
+		para_count = read_command(&command, parameters, prompt);//cmd input
+		if(para_count == -1)//wrong input
+			continue;
 
 		/*int rc = fork();
 	    if (rc < 0) {
@@ -81,6 +165,9 @@ int main(int argc, char *argv[]) {
 		else {
 			//child
 			execve(command, parameters, 0);//execute commad
-	*///}
+	*/}
+
+	free(parameters);
+	free(myshBuffer);	
     return 0;
 }

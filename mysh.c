f53334084e3b50b23dd4e4876f9bc836b3ebf18d
yyr <scsse_yyr2015@126.com>
2017-10-25 12:14:25 +0800

    complete cd and ls (null)

--- mysh.c
+++ mysh.c
@@ -1,10 +1,12 @@
 //head file
 #include <stdio.h>//feof();
-#include <stdlib.h>//exit()
-#include <unistd.h>//fork();getpid();close();execvp()
+#include <stdlib.h>//exit();getenv();
+#include <unistd.h>//fork();getpid();close();execvp();chdir();stat();
 #include <string.h>//strlen();strcmp();
-#include <sys/types.h>//getuid()
+#include <sys/types.h>//getuid();opendir();readdir();closedir();
 #include <pwd.h>//getpwuid();
+#include <dirent.h>
+#include <sys/stat.h>
 
 //define
 #define MAX_PROMPT 513
@@ -12,19 +14,19 @@
 #define MAX_PATH 513
 #define MAX_CMD 515//512+'\n'
 #define MAX_ARG 21
+#define MAX_CNT 1024
 
 #define DEBUG
 
 
 char *myshBuffer;
 
-char error_message[30] = "An error has occurred\n";
-
 #define BACKGROUND 			1
 #define IN_REDIRECT 		2
 #define OUT_REDIRECT 		4
 #define OUT_REDIRECT_APPEND	8
 #define IS_PIPED 			16
+
 struct CMD_INFO
 {
 	int flag;
@@ -33,6 +35,16 @@ struct CMD_INFO
 	char* cmd2;
 	char** parameters2;
 };
+
+void printError() {
+	char error_message[30] = "An error has occurred\n";
+	write(STDERR_FILENO, error_message, strlen(error_message));   	
+    return;
+}
+
+int cmp(const void *a, const void *b) {
+	return strcmp((char*)a,(char*)b);
+}
 
 void type_prompt(char* prompt) {
 	struct passwd *pwd;
@@ -57,12 +69,11 @@ void type_prompt(char* prompt) {
 
     length += sprintf(prompt+length, " mysh> ");
     //puts(prompt);
+    printf("%s",prompt);
     return;
 }
 
 int read_command(char **command,char **parameters,char *prompt) {
-	printf("%s",prompt);
-    
     char* cmd = fgets(myshBuffer,MAX_CMD,stdin);
     
     #ifdef db2_0
@@ -73,7 +84,7 @@ int read_command(char **command,char **parameters,char *prompt) {
     	//scanf("%*[^\n]%*c");
     	//fflush(stdin);
     	rewind(stdin);
-    	write(STDERR_FILENO, error_message, strlen(error_message));   	
+    	printError();
     	return -1;
     }
 
@@ -153,10 +164,96 @@ int read_command(char **command,char **parameters,char *prompt) {
         printf("[%s]\n", parameters[i]);
     #endif
 
-    if(strcmp(parameters[0],"exit") != 0)
-	    return count;
-	else
-		return -2;
+    return count;
+}
+
+/*int cmd_analysis(char **parameters,int para_count,struct CMD_INFO *info) {
+	//init
+	info->flag = 0;
+    info->input = NULL;
+    info->output = NULL;
+    info->cmd2 = NULL;
+    info->parameters2 = NULL;
+
+    if(strcmp(parameters[para_count-1], "&"))
+
+}*/
+
+int built_in_cd(char **parameters) {
+	char *cd_path = NULL;
+	char *home;
+	if(parameters[1] == NULL) {//cd (null)
+    	home = getenv("HOME");
+    	cd_path = malloc(strlen(home+1));
+    	strcpy(cd_path, home);
+    }
+	else {
+		cd_path = malloc(strlen(parameters[1]+1));
+		strcpy(cd_path, parameters[1]);//store path
+	}
+	//printf("%d\n",chdir(cd_path));
+	if(chdir(cd_path) != 0)
+		printError();
+	free(cd_path);
+	return 0;
+}
+
+int built_in_pwd() {
+	char *cwd = NULL;
+	cwd = malloc(MAX_PATH);
+	getcwd(cwd, MAX_PATH);
+	printf("%s\n", cwd);
+	return 0;
+}
+
+int built_in_ls(char **parameters) {
+	DIR *dir;
+    char str[MAX_NAME];
+    char list[MAX_CNT][MAX_NAME];
+    int cnt = 0, i;
+
+    struct dirent *rent;//struct
+    
+    if(parameters[1] == NULL)//ls (null)
+    	parameters[1] = ".";
+
+    dir = opendir(parameters[1]);
+    if (dir == NULL) {  
+        fprintf(stderr, "Can`t open directory %s\n", parameters[1]);  
+        return -1;  
+    }
+
+	//char *p = *list;
+	while((rent=readdir(dir)) != NULL){//read
+    	strcpy(str, rent->d_name);
+    	if(str[0] == '.')
+        	continue;
+    	if(!str)
+        	continue;
+    	strcpy(list[cnt++], str);
+    	printf("%d %s\n", cnt,list[cnt-1]);
+    }
+    printf("------------\n");
+    qsort(list, cnt, sizeof(list[0]), cmp);
+    for(i = 0; i < cnt; i++)
+        printf("%d %s\n", i,list[i]);
+    closedir(dir);
+    return 0;
+}
+
+int built_in_command(char *command, char **parameters) {
+	//exit
+	if(strcmp(command,"exit")==0 || strcmp(command,"q")==0)
+        exit(0);
+    else if(strcmp(command, "cd") == 0)
+    	built_in_cd(parameters);
+    else if(strcmp(command, "pwd") == 0)
+    	built_in_pwd();
+    else if(strcmp(command, "ls") == 0)
+    	built_in_ls(parameters);
+    else
+    	return 0;
+    return 0;
 }
 
 int main() {
@@ -164,20 +261,20 @@ int main() {
 	char *command = NULL;
 	char **parameters;
 	int para_count = 0;
+	struct CMD_INFO info;
 
 	myshBuffer = malloc(sizeof(char)*MAX_CMD);
 	parameters = malloc(sizeof(char*)*MAX_ARG);
-	
-	while(1) {
+
+	while(1) {	
 		type_prompt(prompt);//generate prompt
 		para_count = read_command(&command, parameters, prompt);//cmd input
 		if(para_count == -1)//wrong input
 			continue;
-		if (para_count == -2)
-			break;
 		para_count -= 1;
-		cmd_analysis(parameters,para_count,&info);
-
+		//cmd_analysis(parameters,para_count,&info);
+		if(built_in_command(command,parameters))
+            continue;
 		/*int rc = fork();
 	    if (rc < 0) {
 	    	//fork failed then exit
@@ -190,7 +287,7 @@ int main() {
 		}
 		else {
 			//child
-			execve(command, parameters, 0);//execute commad
+			execve(command, parameters, 0);//execute command
 	*/}
 
 	free(parameters);
